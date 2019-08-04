8391593ed850a4bd323b782ef49968185985ef3a
Anubhav-Vats <onbitsyn@gmail.com>
2019-08-05 02:49:41 +0530

    Serving static files

--- index.js
+++ index.js
@@ -1,16 +1,14 @@
 const express = require('express');
 const http = require('http');
 const app = express();
+const morgan = require('morgan');
 
 const hostname = 'localhost';
-const port = 3000;
 
-app.use( (req,res,next) =>{
-    console.log(req.headers);
-    res.statusCode = 200;
-    res.setHeader('Content-Type','text/html');
-    res.end('<html><body><h1>Welcome .....<br></br>express server</h1></body></html>');
-});
+const port = 3000;
+app.use(morgan('dev'));
+app.use(express.static(__dirname + '/public'));
+//this is something cool .....now i can response a static file...
 
 const server = http.createServer(app);
 
